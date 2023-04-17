08c6a45de0081c6c067cb5fc52361bf204916b5a
Kari Adilova <kariadilovaadilova7@gmail.com>
2023-04-17 23:24:53 +0500

    feat: implement stack task

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.array = [];
+  }
+
+  push(element) {
+    this.array.push(element); 
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let lastElement = this.array[this.array.length - 1];
+    this.array.pop();
+    return lastElement;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array[this.array.length - 1]
   }
 }
 
